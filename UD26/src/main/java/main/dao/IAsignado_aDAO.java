98c433f3b0ebcb19c2c6c88e9ff27e8ac0721485
Eloy Altozano Meijer <93398643+EloySPAIN@users.noreply.github.com>
2022-02-28 10:23:01 +0100

    Ej2

--- UD26/src/main/java/main/dao/IAsignado_aDAO.java
+++ UD26/src/main/java/main/dao/IAsignado_aDAO.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import main.dto.Asignado_a;
 
-public interface IAsignado_a extends JpaRepository<Asignado_a, Integer> {
+public interface IAsignado_aDAO extends JpaRepository<Asignado_a, Integer> {
 
 }
